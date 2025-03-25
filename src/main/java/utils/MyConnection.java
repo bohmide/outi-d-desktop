fc3b9a5cb163c81ac8b86dde0ab30679b682744a
bohmide <ahmedbousnina02@gmail.com>
2025-03-25 02:17:31 +0100

    connection conf

--- src/main/java/utils/MyConnection.java
+++ src/main/java/utils/MyConnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class MyConnection {
-    String url = "jdbc:mysql://localhost:3306/test";
+    String url = "jdbc:mysql://localhost:3306/outi-d";
     String login ="root";
     String pwd="";
     public static MyConnection instance ;
