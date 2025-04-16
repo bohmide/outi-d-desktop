7e6c9eec4de277ee089539d6387385e228c5e111
bohmide <ahmedbousnina02@gmail.com>
2025-04-16 21:40:22 +0100

    crud

---
+++ src/main/java/controller/FirstScene.java
@@ -0,0 +1,24 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+    public class FirstScene {
+
+        @FXML
+        private Button bt;
+
+        @FXML
+        private TextField tf1;
+
+        @FXML
+        private TextField tf2;
+
+        @FXML
+        void savePersonne(ActionEvent event) {
+
+        }
+
+    }
+
